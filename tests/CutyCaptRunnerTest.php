f7c02941757dd6c284a7834ba1543c7b3f34ddcf
c9s <cornelius.howl@gmail.com>
2012-04-20 02:20:00 +0800

    add test and vendor.sh

---
+++ tests/CutyCaptRunnerTest.php
@@ -0,0 +1,10 @@
+<?php
+
+class CutyCaptRunnerTest extends PHPUnit_Framework_TestCase
+{
+    function test()
+    {
+        $runner = new CutyCaptRunner('');
+    }
+}
+
---
+++ vendor.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# for MacOS
+svn co https://cutycapt.svn.sourceforge.net/svnroot/cutycapt
+cd cutycapt/CutyCapt
+qmake
+make 
